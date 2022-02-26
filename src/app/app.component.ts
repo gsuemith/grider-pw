36c8f434ba6ebef7301c4a72f7908ec57a4ab14e
Garrick Suemith <gsuemith@zotecpartners.com>
2022-02-25 23:03:07 -0500

    app functionality complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gen-password';
+  length: number = 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  password: string = '';
+
+  onChangeLength(target: EventTarget) {
+    this.length = parseInt((target as HTMLInputElement).value)
+
+    if (isNaN(this.length)) {
+      this.length = 0;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '0123456789'
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*()-=_'
+
+    let validChars = ''
+    validChars += this.includeLetters ? letters : ''
+    validChars += this.includeNumbers ? numbers : ''
+    validChars += this.includeSymbols ? symbols : ''
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length)
+      generatedPassword += validChars[index]
+    }
+
+    this.password = generatedPassword
+  }
+
 }
